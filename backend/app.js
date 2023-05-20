b73b0e5902b62bdfb80a52579cd9047a0d7f5b9f
Syed-Hashma-au52 <syedhashma777@gmail.com>
2023-05-20 10:41:48 +0530

    project

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,32 @@
-import express from 'express'; 
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user_routes";
+import adminRouter from "./routes/admin_routes";
+import movieRouter from "./routes/movie_routes";
+import bookingsRouter from "./routes/booking_routes";
+dotenv.config();
 
 const app = express();
 
-app.listen(5000,() => {
-    console.log('conntected to localhost Port ${5000}');
-})
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
+
+
+mongoose
+   .connect
+    (`mongodb+srv://itshashma18:${process.env.MONGODB_PASSWORD}@cluster0.m1x8svz.mongodb.net/?retryWrites=true&w=majority`
+    )
+
+.then(() =>
+app.listen(8000, () =>
+  console.log("Connected To Database And Server is running")
+)
+)
+.catch((e) => console.log(e));
+
+
+// K7vNtmsDfk9K99Tj
---
+++ backend/routes/movie_routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  addMovie,
+  getAllMovies,
+  getMovieById,
+} from "../controllers/movie_controller";
+
+const movieRouter = express.Router();
+
+movieRouter.get("/", getAllMovies);
+movieRouter.get("/:id", getMovieById);
+movieRouter.post("/", addMovie);
+
+export default movieRouter;
